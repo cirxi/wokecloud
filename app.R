e2fd4df5352d18d7253c808b2b26b6c5b711669f
Utek <utek@outlook.com>
2022-06-12 15:10:23 +0800

    adding sidebar and link to github page

--- app.R
+++ app.R
@@ -5,24 +5,32 @@ library(tidyverse)
 library(wordcloud)
 library(tidytext)
 
-# UI
-ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody(
-    
-    # Application title
-    titlePanel("WOKECLOUD GENERATOR"),
-    
-    # twitter handle input
-    textInput("handle", "Twitter Handle HERE?", placeholder = "e.g. charlesmurray"),
-    
-    # wordcloud
-    plotOutput("wordcloud", width = "700px", height = "700px"),
+# UI header
+header <- dashboardHeader(title = "Project WOKECLOUD")
+
+# UI sidebar
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Wokecloud", tabName = "page_wordcloud", icon = icon("cloud")),
+    menuItem("Github Repo", icon = icon("github"), href = "https://github.com/cirxi/wokecloud")
+    )
+  )
+
+# UI body
+body <- dashboardBody(
   
-    # generate button
-    actionButton("execute", "Generate", class = "btn-success btn-block"))
-)
+  tabItem(tabName = "page_wordcloud",
+          # Application title
+          titlePanel("WOKECLOUD GENERATOR"),
+          # twitter handle input
+          textInput("handle", "Twitter Handle HERE?", placeholder = "e.g. charlesmurray"),
+          # wordcloud
+          plotOutput("wordcloud", width = "700px", height = "700px"),
+          # generate button
+          actionButton("execute", "Generate", class = "btn-success btn-block")),
+  )
+
+ui <- dashboardPage(header, sidebar, body)
 
 # Server
 server <- function(input, output, session) {
