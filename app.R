1ef08a53c675653363e9cdab40615d5004cce8dd
Utek <utek@outlook.com>
2022-06-12 21:41:26 +0800

    adding notification for plot generation

--- app.R
+++ app.R
@@ -70,12 +70,17 @@ server <- function(input, output, session) {
       count(word, sort = TRUE)
   })
   
+  observeEvent(input$generate, {
+    showNotification("Generating...", duration = NULL, closeButton = FALSE, id = "notification_plot", type = "message")
+  })
+  
   # wordcloud script
   output$wordcloud <- renderPlot({
     req(input$grab, input$generate)
     wordcloud(words = tweets_wordcloud()$word, freq = tweets_wordcloud()$n,
                                            min.freq = 10, max.words = 50, colors = brewer.pal(8, "Dark2"),
                                            scale = c(3, 0.5))
+    removeNotification("notification_plot")
     })
   
 }
